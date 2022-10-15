6ebc3db706a9338a78cd0b623c678968f80325ab
B G <mahi.n.suhi@gmail.com>
2022-10-15 10:09:36 +0100

    completed excercise 5

--- src/main/java/com/serenitydojo/Hamster.java
+++ src/main/java/com/serenitydojo/Hamster.java
@@ -1,28 +1,22 @@
 package com.serenitydojo;
 
-public class Cat extends Pet {
+public class Hamster extends Pet {
 
     private String favoriteToy;
 
-    public Cat(String name, String favoriteToy, int age) {
+    public Hamster(String name, int age, String favoriteToy) {
         super(name, age);
         this.favoriteToy = favoriteToy;
-
     }
 
     public String getFavoriteToy() {
         return favoriteToy;
     }
 
-    public void setFavoriteToy(String favoriteToy) {
-        this.favoriteToy = favoriteToy;
-    }
-
     @Override
     public String play() {
 
-        return "plays with string";
-
+        return "runs in wheel";
     }
 
 }
